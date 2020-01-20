2e0b80835efd31fd5260c5ec5e9d51e55068e635
oneofthepebble <52684117+oneofthepebble@users.noreply.github.com>
2020-01-20 15:47:16 +0900

    Update Gmail.java

--- src/main/java/com/project/gmail/Gmail.java
+++ src/main/java/com/project/gmail/Gmail.java
@@ -16,8 +16,8 @@ public class Gmail {
     public static void gmailSendID(String name,String email,String id) {
 
 
-        final String user = "chlwoaud1234@gmail.com"; // 네이버일 경우 네이버 계정, gmail경우 gmail 계정
-        final String password = "WindowXP506@";   // 패스워드
+        final String user = ""; // 네이버일 경우 네이버 계정, gmail경우 gmail 계정
+        final String password = "";   // 패스워드
 
         // SMTP 서버 정보를 설정한다.
         Properties prop = new Properties();
@@ -63,8 +63,8 @@ public class Gmail {
 
     ////////////////임시비번 이메일
     public static void gmailSendPWD(String name,String email,String pwd) {
-        final String user = "chlwoaud1234@gmail.com"; // 네이버일 경우 네이버 계정, gmail경우 gmail 계정
-        final String password = "WindowXP506@";   // 패스워드
+        final String user = ""; // 네이버일 경우 네이버 계정, gmail경우 gmail 계정
+        final String password = "";   // 패스워드
 
         // SMTP 서버 정보를 설정한다.
         Properties prop = new Properties();
